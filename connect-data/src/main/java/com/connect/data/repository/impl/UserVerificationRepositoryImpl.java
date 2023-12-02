dd68a7e2a6a6e623af41f64582c0f6a29d8ec89b
leduoayng <leduoyang1995@gmail.com>
2023-12-02 23:54:12 +0800

    init signup process and implement email verification

--- connect-data/src/main/java/com/connect/data/repository/impl/UserVerificationRepositoryImpl.java
+++ connect-data/src/main/java/com/connect/data/repository/impl/UserVerificationRepositoryImpl.java
@@ -46,11 +46,20 @@ public class UserVerificationRepositoryImpl implements IUserVerificationReposito
 
     @Override
     public void createUserVerification(UserVerification userVerification) {
-        userVerificationDao.expireCodeByEmail(userVerification.getEmail());
-
-        int affected = userVerificationDao.createUserVerification(userVerification);
+        int affected = userVerificationDao.expireCodeByEmail(userVerification.getEmail());
         if (affected <= 0) {
-            throw new ConnectDataException(ConnectErrorCode.USER_CREATE_EXCEPTION);
+            throw new ConnectDataException(
+                    ConnectErrorCode.USER_VERIFICATION_CREATE_EXCEPTION,
+                    "expire previous code by email failed"
+            );
+        }
+
+        affected = userVerificationDao.createUserVerification(userVerification);
+        if (affected <= 0) {
+            throw new ConnectDataException(
+                    ConnectErrorCode.USER_VERIFICATION_CREATE_EXCEPTION,
+                    "create email verification code failed"
+            );
         }
     }
 
