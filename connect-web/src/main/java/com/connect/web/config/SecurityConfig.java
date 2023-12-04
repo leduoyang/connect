927c2db4b39f0beb8de534cda0d1f70cc03c4b07
leduoayng <leduoyang1995@gmail.com>
2023-12-04 16:22:44 +0800

    update security config

--- connect-web/src/main/java/com/connect/web/config/SecurityConfig.java
+++ connect-web/src/main/java/com/connect/web/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.connect.web.config;
 
-import com.connect.data.entity.User;
 import com.connect.web.filter.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -16,7 +15,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
