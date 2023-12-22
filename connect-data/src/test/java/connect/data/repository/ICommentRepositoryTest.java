713dccdad6c67b8a50672740d9ac945388d8e87c
leduoayng <leduoyang1995@gmail.com>
2023-12-23 01:14:29 +0800

    remove repository testing for github ci

--- connect-data/src/test/java/connect/data/repository/ICommentRepositoryTest.java
+++ connect-data/src/test/java/connect/data/repository/ICommentRepositoryTest.java
@@ -1,20 +1,22 @@
-package connect.data.repository;
-
-import com.connect.data.entity.Comment;
-import com.connect.data.repository.ICommentRepository;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest(classes = TestDBApplication.class)
-public class ICommentRepositoryTest {
-    @Autowired
-    private ICommentRepository commentRepository;
-
-    @Test
-    void get_comment_by_comment_id_should_return_target_comment() {
-        Long commentId = 1L;
-        Comment comment = commentRepository.queryCommentById(commentId, "ROOT");
-        System.out.println(comment.toString());
-    }
-}
+//package connect.data.repository;
+//
+//import com.connect.data.entity.Comment;
+//import com.connect.data.repository.ICommentRepository;
+//import org.junit.jupiter.api.Disabled;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest(classes = TestDBApplication.class)
+//public class ICommentRepositoryTest {
+//    @Autowired
+//    private ICommentRepository commentRepository;
+//
+//    @Test
+//    @Disabled
+//    void get_comment_by_comment_id_should_return_target_comment() {
+//        Long commentId = 1L;
+//        Comment comment = commentRepository.queryCommentById(commentId, "ROOT");
+//        System.out.println(comment.toString());
+//    }
+//}
