1dc0b8fa6073fa94c053f80fe36d440b7381223e
leduoayng <leduoyang1995@gmail.com>
2023-12-27 22:04:34 +0800

    disable cors

--- connect-web/src/main/java/com/connect/web/config/SecurityConfig.java
+++ connect-web/src/main/java/com/connect/web/config/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
                 .sessionManagement((session) ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
