88bba9045283ae1d279c6fee7a346bfcdf89ac2c
leduoayng <leduoyang1995@gmail.com>
2023-12-29 14:01:04 +0800

    update EditProfileRequest.

--- connect-api/src/main/java/com/connect/api/user/request/EditProfileRequest.java
+++ connect-api/src/main/java/com/connect/api/user/request/EditProfileRequest.java
@@ -12,10 +12,6 @@ public class EditProfileRequest {
     @Size(min = 1, max = 20, message = "userId can not be blank and must be at most 20")
     private String username;
 
-    @Min(value = 0, message = "status must be at least 1 (0 - public, 1 - semi, 2 - private)")
-    @Max(value = 2, message = "status must be at most 2 (0 - public, 1 - semi, 2 - private)")
-    private Integer status;
-
     @Size(max = 200)
     private String description;
 
--- connect-api/src/test/java/com/connect/api/request/EditProfileRequestValidationTest.java
+++ connect-api/src/test/java/com/connect/api/request/EditProfileRequestValidationTest.java
@@ -21,28 +21,9 @@ public class EditProfileRequestValidationTest {
     public void test_create_editProfileRequest_with_valid_payload_should_pass_validation() {
         EditProfileRequest editProfileRequest = new EditProfileRequest()
                 .setUsername("12345678")
-                .setStatus(1)
                 .setDescription("12345678");
         Set<ConstraintViolation<EditProfileRequest>> violations = validator.validate(editProfileRequest);
         assertTrue(violations.isEmpty(), "Validation should pass for a valid editProfile request");
-    }
-
-    @Test
-    public void test_create_editProfileRequest_with_null_status_should_pass_validation() {
-        EditProfileRequest editProfileRequest = new EditProfileRequest()
-                .setUsername("12345678")
-                .setDescription("12345678");
-        Set<ConstraintViolation<EditProfileRequest>> violations = validator.validate(editProfileRequest);
-        assertTrue(violations.isEmpty(), "Validation should pass for a valid editProfile request");
-    }
-
-    @Test
-    public void test_create_editProfileRequest_with_invalid_status_should_fail_validation() {
-        EditProfileRequest editProfileRequest = new EditProfileRequest()
-                .setStatus(-1)
-                .setDescription("12345678");
-        Set<ConstraintViolation<EditProfileRequest>> violations = validator.validate(editProfileRequest);
-        assertEquals(1, violations.size(), "Validation should fail for a invalid editProfile request");
     }
 
     @Test
@@ -57,7 +38,6 @@ public class EditProfileRequestValidationTest {
     public void test_create_editProfileRequest_with_blank_userId_should_fail_validation() {
         EditProfileRequest editProfileRequest = new EditProfileRequest()
                 .setUsername("")
-                .setStatus(1)
                 .setDescription("12345678");
         Set<ConstraintViolation<EditProfileRequest>> violations = validator.validate(editProfileRequest);
         assertEquals(1, violations.size(), "Validation should fail for a invalid editProfile request");
@@ -67,7 +47,6 @@ public class EditProfileRequestValidationTest {
     public void test_create_editProfileRequest_with_over_max_userId_should_fail_validation() {
         EditProfileRequest editProfileRequest = new EditProfileRequest()
                 .setUsername("a".repeat(21))
-                .setStatus(1)
                 .setDescription("12345678");
         Set<ConstraintViolation<EditProfileRequest>> violations = validator.validate(editProfileRequest);
         assertEquals(1, violations.size(), "Validation should fail for a invalid editProfile request");
